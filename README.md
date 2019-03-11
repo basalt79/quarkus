6acdaa315245ed97d0965587dcb7c230068826b4
georg <georg@noenv.com>
2019-03-11 10:06:39 +0100

    initial commit on this tryout

---
+++ README.md
@@ -0,0 +1,1 @@
+try https://quarkus.io/
---
+++ src/test/java/at/aigner/quarkus/NativeEchoIT.java
@@ -0,0 +1,9 @@
+package at.aigner.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeEchoIT extends EchoTest {
+
+    // Execute the same tests but in native mode.
+}
